f0128dc374febfd6f6734ddbfee5b715d4d73b66
austinmpask <austinmpask@gmail.com>
2024-01-27 20:49:43 -0800

    init

--- fetch-requests.js
+++ fetch-requests.js
@@ -4,16 +4,35 @@ Paste your code for fetch requests here once you finish each phase.
 */
 
 /* ============================== Phase 1 ============================== */
+const url1 = "/products";
+
+const body = new URLSearchParams({
+  name: "Caribbean Delight Coffee",
+  description: "Made by Manatee Coffee",
+  price: 11.99,
+  categories: "grocery",
+});
+
+const headers = {
+  "Content-Type": "application/x-www-form-urlencoded",
+};
+
+const options1 = {
+  method: "POST",
+  body: body,
+  headers: headers,
+};
+const response = await fetch(url1, options1);
+console.log(response);
+console.log(response.status);
+console.log(response.headers.get("content-type"));
+console.log(response.url);
 
 // Your code here
-
-
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
-
-
 
 /* ============================== Phase 3 ============================== */
 
